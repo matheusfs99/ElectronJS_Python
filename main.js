06e445f28a26f6ab8ca136a2b84bcaa90c95be29
Matheus Farias <matheusfarias009@hotmail.com>
2020-11-18 11:37:10 -0300

    (01)index html com pagina de login

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const {app, BrowserWindow} = require('electron')
 
 function createWindow() {
-  window = new BrowserWindow({width: 800, height: 600})
-  window.loadFile('index.html')
+  window = new BrowserWindow({width: 800, height: 600});
+  window.loadFile('index.html');
+}
 
+function call_python(){
   var python = require('child_process').spawn('python', ['./test_selenium.py']);
   python.stdout.on('data',function(data){
       console.log("data: ",data.toString('utf8'));
   });
+}
+
+function login(){
+  var login = document.getElementById('login').value();
+  var senha = document.getElementById('senha').value();
+  console.log(login + senha)
+  if (login == 'aaa' && senha == '123'){
+    call_python()
+  }
 }
 
 app.on('ready', createWindow)
